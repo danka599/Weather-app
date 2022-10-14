fae89871d0b8f918ed719c9749b16ea0beb71af3
danka599 <115226085+danka599@users.noreply.github.com>
2022-10-14 11:08:15 +0200

    Added new city on reload

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function showTemperature(response) {
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  let temperatureElement = document.querySelector("#temperature");
+  let showTemp = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(showTemp);
   document.querySelector("#chosen-city").innerHTML = response.data.name;
   document.querySelector("#country").innerHTML = response.data.sys.country;
   document.querySelector("#actual-weather").innerHTML =
@@ -27,19 +27,24 @@ function geoLocation(e) {
   e.preventDefault();
   navigator.geolocation.getCurrentPosition(currentPosition);
 }
+
+function search(city) {
+  let apiKey = "f3009e4852fa0a079dab291dabf020c4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
 function enterCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector(`#input-city`);
-
-  let apiKey = "f3009e4852fa0a079dab291dabf020c4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+  search(cityInput.value);
 }
+search("Amsterdam");
 let form = document.querySelector(`#city-form`);
 form.addEventListener(`submit`, enterCity);
+
 let button = document.querySelector("#button");
 button.addEventListener("click", geoLocation);
-//challenge 2
+//change time
 
 let date = new Date();
 let days = [
@@ -53,26 +58,35 @@ let days = [
 ];
 let day = days[date.getDay()];
 let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let changeDay = document.querySelector(`#changeDay`);
 changeDay.innerHTML = `${day}`;
 let changeTime = document.querySelector(`#changeTime`);
 changeTime.innerHTML = `${hours}:${minutes}`;
 
-//challenge 3
+//Unit change
 
-function changeTemperature(event) {
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = "66";
+function changeTemperatureToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (temperatureElement.innerHTML * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
-
-let fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", changeTemperature);
 
 function changeTemperatureToCelsius(event) {
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = "16";
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = celsiusTemp;
 }
+let celsiusTemp = null;
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", changeTemperatureToFahrenheit);
 
 let celsius = document.querySelector("#celsius");
 celsius.addEventListener("click", changeTemperatureToCelsius);
